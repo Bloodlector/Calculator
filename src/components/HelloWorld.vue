030e835369d04fbf232d8fc0a3402a1b7957eb7f
Kris <bloodlector@mail.ru>
2022-02-26 13:28:16 +0300

    homework 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,27 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <hr/>
+    <!-- <button v-on:click="doThat('hello', $event)">click me</button>
+    <button @click="doThat('hello', $event)">click me2</button>
+    <input type="text" @blur="onValidate"/> -->
+    <div class="display">
+      <input v-model="operand1" type="number"><span>Число 1</span>
+      <input v-model="operand2" type="number"><span>Число 2</span><br>
+      <span>Результат</span>= {{ result }}
+    </div>
+
+    <div class="keyboard">
+      <button @click='result = +operand1 + +operand2'>+</button>
+      <button @click='result = +operand1 - +operand2'>-</button>
+      <button @click='divide(operand1, operand2)'>/</button>
+      <button @click='result = +operand1 * +operand2'>*</button><br>
+      <button @click='multiply(operand1, operand2)'>Степень</button><span> Число 1 возвести в степень равную числу 2</span>
+      <button @click='divideP(operand1, operand2)'>Int /</button><span> целочисленное деления числа 1 на число 2</span>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -35,6 +30,52 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+    
+  },
+  data(){
+    return{
+      message: "Hello Vue!",
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+    }
+  },
+  methods: {
+    doThat(str, evnt){
+      console.log('click', str, evnt)
+    },
+    onValidate(){
+      console.log('Validation!')
+    },
+    multiply(){
+if (this.operand2==0){
+  alert("степень должна быть больше 0!")
+} else {
+        let op = this.operand1;
+      let res = this.operand1;
+      for(let i=1; i<this.operand2; i++){
+      this.operand1 = res * this.operand1;
+      this.result = this.operand1;
+      }
+      this.operand1 = op;
+}
+    },
+    divideP(){
+      if (this.operand2==0){
+        alert("Делить на 0 нельзя!");
+      }
+      else {
+      this.result = parseInt(this.operand1 / this.operand2)
+      }
+    },
+        divide(){
+      if (this.operand2==0){
+        alert("Делить на 0 нельзя!");
+      }
+      else {
+      this.result = this.operand1 / this.operand2;
+      }
+    }
   }
 }
 </script>
